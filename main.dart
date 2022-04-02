8cf1e5d289d973f03e0d89bc4f7e75b562a59703
Muhsin Zafar <83918640+muhsinzafar@users.noreply.github.com>
2022-04-03 00:15:45 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+void main() {
+  // Q1: Write a program to caclulate the factorial of given number.
+  var a = 5;
+  var b = 5 * 4;
+  var c = 5 * 3;
+  var d = 5 * 2;
+  var e = 5 * 1;
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+// / Q2: Write a program to make such a pattern like a pyramid with a number
+// which will repeat the number in same row
+  // nh araha sir but I am trying
+
+  // // Q3; Write a program to find out the sum of series
+  var sum = 0;
+  var given_list = [1, 5, 9, 13, 17, 21, 25, 29, 33, 37];
+
+  for (var i = 0; i < given_list.length; i++) {
+    sum += given_list[i];
+  }
+  print("Sum : ${sum}");
+}
+
+// Q4: Write a program to find majority element of an array
+List std = [1, 3, 3, 7, 4, 3, 2, 3, 3];
+  
+    // std.join("");
+    // // std.removeAt(2);
+    // // std.removeAt(4);
+    // // std.removeAt(6);
+    // // std.removeLast();
+    // // std.sort();
+    // print(std);
+// Q5: Write a program to find the minimum distance b/w two numbers in given array
+//   var Listn = [7, 9, 5, 11, 7, 4, 12, 6, 2, 11];
+
+// // Write a program to check whether a character is an alphabet, digit or special charachter
+// List std2= ["Mohsin", "@",664];
+    
+  
